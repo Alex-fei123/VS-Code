a8493f1d5975f54c8c71bcb0c6b8e9e2bd6292c8
Alex_fei <1284961742@qq.com>
2024-11-04 21:05:00 +0800

    d

---
+++ action.js
@@ -0,0 +1,10 @@
+function name(params) {
+    if (condition) {
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            while (condition) {
+                
+            }
+        }
+    }
+}
